86228b6b4561a507e5abf510f13d2dcad6b6a5d4
Lucas Rocha <lucasbezerrar@gmail.com>
2024-05-05 10:05:51 -0300

    Add restante da aula de introdução

--- origamid/react-completo/javascript.js
+++ origamid/react-completo/javascript.js
@@ -1,4 +1,5 @@
 // Destructuring
+// objetos
 function handleMouseMove(event) {
   const clientX = event.clientX;
   const clientY = event.clientY;
@@ -18,3 +19,101 @@ function handleMouseMove({ clientX, clientY }) {
 }
 
 document.documentElement.addEventListener('mousemove', handleMouseMove);
+
+// arrays
+const frutas = ['Banana', 'Uva'];
+const [fruta1, fruta2] = frutas;
+// fruta1 = banana
+// fruta2 = Uva
+
+const useQuadrado = [
+  4,
+  function (lado) {
+    return 4 * lado;
+  },
+];
+const [lados, area] = useQuadrado;
+
+// Rest
+function showList(empresa, ...clients) {
+  clients.forEach((client) => {
+    console.log(client, empresa);
+  });
+}
+
+showList('Google', 'André', 'Pedro', 'João');
+
+// Spred
+// Arrays
+const numeros = [1, 4, 9, 2, 3, 6, 20];
+Math.max(...numeros); // retorna 20
+
+// Transformar ArrayLike and Array
+const items = document.querySelectorAll('li');
+
+// items.map() não existe, agora [...items]
+// é uma nova array, com cada elemento de items.
+[...items].map((item) => (item.innerText = 'Teste'));
+
+// Objetos
+const carro = {
+  cor: 'azul',
+  portas: 4,
+  ano: 2020,
+};
+
+const cloneCarro = { ...carro };
+const carroEsportivo = { turbo: true, ...carro };
+
+// Fetch
+fetch('https://ranekapi.origamid.dev/json/api/produto')
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json);
+  });
+
+// Async Await
+async function fetchProdutos(url) {
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+}
+
+fetchProdutos('https://ranekapi.origamid.dev/json/api/produto');
+
+// Arrays (Map e Filter)
+const precos = [
+  'Crédito',
+  'R$ 200',
+  'R$ 400',
+  'Contas Pagar',
+  'R$ 300',
+  'R$ 400',
+  'Meus dados',
+];
+
+// Retorna uma array nova, que contem os items em
+// que o retorno da função for verdadeiro
+const precosFiltro = precos.filter((preco) => preco.includes('R$'));
+
+// Retorna uma nova array, modificada com o
+// retorno de cada item da função
+const precoNumeros = precosFiltro.map((preco) =>
+  Number(preco.replace('R$ ', '')),
+);
+
+// Expressões
+const grupoA = 100;
+const grupoB = 300;
+const vencedor = grupoA > grupoB ? 'Grupo A Venceu' : 'Grupo B Venceu';
+
+const numeros = [2, 3, 4, 5, 6];
+const total = numeros.filter((numero) => numero > 4);
+
+const active = true;
+const button = active && 'Botão está ativo';
+
+// JSX
+<button onClick={(event) => event.target.classList.add('active')}>
+  Button
+</button>
